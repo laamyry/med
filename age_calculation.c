680939779908edec53459f11ed89e729293a0cc7
laamyry <slamiri10@gmail.com>
2023-03-10 12:37:10 -0800

    laamyry

---
+++ age_calculation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+   int main(){
+	int x,age;
+	int y = 2023;
+
+	printf("enter your birth year: \n");
+	scanf("%d",&x);
+	age = y-x;
+	printf("your age is: %d old \n", age);
+		
+	return 0;
+	
+	
+	
+	}
